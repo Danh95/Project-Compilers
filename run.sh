1d39cc323a3b4144279d5838b59269b306809d06
Lorin-Teugels <Lorin.teugels@hotmail.com>
2017-04-23 21:33:36 +0200

    run.sh
    New try

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+java -jar antlr-4.7-complete.jar -Dlanguage=Python3 grammarC.g4 -visitor
+python3 ./src/parser.py ./src/test/test1.c
+python3 ./src/parser.py ./src/test/test2.c
+python3 ./src/parser.py ./src/test/test3.c
+python3 ./src/parser.py ./src/test/test4.c
+python3 ./src/parser.py ./src/test/test5.c
+python3 ./src/parser.py ./src/test/test6.c
